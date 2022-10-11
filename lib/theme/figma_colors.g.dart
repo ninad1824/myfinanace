a8d0cc3a0631870f9e21c81748717ea073df760a
Parabeac-Nest <admin@parabeac.com>
2022-10-11 17:55:46 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/figma_colors.g.dart
@@ -0,0 +1,26 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class FigmaColors {
+
+static const Color lightLabels1Primary = Color(0xff000000);
+
+static const Color iOSSystemLabelsDarkPrimary = Color(0xffffffff);
+/// Black Color
+static const Color lightBase = Color(0xff000000);
+
+static const Color 232D42TextColor1 = Color(0xff232d42);
+
+static const Color colorShades03 = Color(0xff818181);
+
+static const Color colorPrimary05 = Color(0xff1e90ff);
+
+static const Color colorTint05 = Color(0xfff7f7f7);
+
+static const Color iOSSystemFillsLightQuarternary = Color(0xff747480);
+}
